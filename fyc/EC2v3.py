11b4e210083971cd3f56537b8ecc0904c538b14c
W1sh222 <1400151410@sjtu.edu.cn>
2023-07-16 22:21:13 +0800

    connectingtry

---
+++ fyc/EC2v3.py
@@ -0,0 +1,161 @@
+from flask import Flask, request, jsonify
+import subprocess
+import boto3
+import json
+import threading
+import requests
+import queue
+
+step_request_queue = queue.Queue()           # 请求队列
+step_response_queue = queue.Queue()          # 响应队列
+restart_request_queue = queue.Queue()        # 重启请求队列
+restart_response_queue = queue.Queue()       # 重启响应队列
+
+overall_average_events = {}                  # Event objects for overall average calculation
+groups_number = []                         
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def handle_post_request():
+    request_data = request.get_json()
+    if request_data.get('request') == 'average_step':
+        group_id = request_data.get('group_id')
+        step_request_queue.put(request_data)  
+
+        print(group_id)
+
+        # Wait for overall average calculation to complete
+        overall_average_events[group_id].wait()
+
+        # 获取响应数据
+        response_data = step_response_queue.get()
+
+    elif request_data.get('request') == 'restart':
+        restart_request_queue.put(request_data)  # Put the restart request data into the restart request queue
+        response_data = restart_response_queue.get()  # Wait to get the response data
+
+    print(response_data)
+
+    return jsonify(response_data)
+
+
+def restart_worker():
+    while True:
+        request_data = restart_request_queue.get()  # Get the restart request data from the restart request queue
+        # Process the restart request and generate the response data
+        response = restart(request_data)
+        restart_response_queue.put(response)        # Put the response data into the restart response queue
+
+
+def step_worker(group_id):
+    overall_average_event = overall_average_events[group_id]
+    response_queue = queue.Queue()  # 创建一个临时的响应队列用于存储当前步骤的响应
+
+    while True:
+        request_data = step_request_queue.get()     # Get the step request data from the step request queue
+
+        print("receive one")
+
+        response_queue.put(request_data)             # 将请求数据放入响应队列
+
+        # Check if all Lambda requests have arrived for the group
+        if step_request_queue.qsize() == groups_number[group_id]: 
+            print(group_id,"done")
+            step_average_all = calculate_overall_average(response_queue, group_id)
+
+            response_data = {
+                'average_step' : step_average_all,
+            }
+            
+            for _ in range(groups_number[group_id]):
+                step_response_queue.put(response_data)
+                # response_queue.get()
+
+            overall_average_event.set()
+
+    
+
+def restart(data):
+    # Get instance URL and other necessary data from 'data' dictionary
+    instance_url = get_instance_url()
+
+    file_name = data.get('file_name')
+    request = data.get('request')
+    bucket_name = data.get('bucket_name')
+    params_file_name = data.get('params_file_name')
+    group_id = data.get('group_id')
+    epochs_done = data.get('epochs_done')
+    ip_address = data.get('ip_address')
+    port = data.get('port')
+
+    print(file_name)
+
+    # Prepare payload for Lambda invocation
+    payload = {
+        'if_restart' : True,
+        'ip_address' : ip_address,
+        'port' : port,
+        'bucket_name' : bucket_name,
+        'params_file_name' : params_file_name,
+        'group_id' : group_id,
+        'epochs_done' : epochs_done
+    }
+
+    # Invoke Lambda function asynchronously
+    lambda_client = boto3.client('lambda', region_name='us-west-2')
+    lambda_client.invoke(FunctionName=file_name,
+                         InvocationType='Event',
+                         Payload=json.dumps(payload))
+
+    print(instance_url)
+
+    response = {
+        'message': 'CLOSE'
+    }
+
+    return response
+
+
+def calculate_overall_average(response_queue, group_id):
+    averages = []
+
+    while not response_queue.empty():
+        response = response_queue.get()
+        step = response.get('step')
+        averages.append(step)
+
+    # 转置操作
+    transposed_arrays = zip(*averages)
+
+    # 计算平均值
+    overall_average = [sum(column) / len(column) for column in transposed_arrays]
+
+    return overall_average
+
+
+def get_instance_url():
+    metadata_url = "http://169.254.169.254/latest/meta-data/public-ipv4"
+    response = requests.get(metadata_url)
+    if response.status_code == 200:
+        instance_ip = response.text
+        instance_url = f"http://{instance_ip}"
+        return instance_url
+    else:
+        return None
+
+if __name__ == '__main__':
+    # Create and start the thread for processing restart requests
+    restart_thread = threading.Thread(target=restart_worker)
+    restart_thread.start()
+
+    total_groups = 2 # set the number of training groups 
+
+    # Create and start the thread for processing step requests for each group
+    for group_id in range(total_groups):
+        groups_number.append(2)
+        overall_average_events[group_id] = threading.Event()
+        step_thread = threading.Thread(target=step_worker, args=(group_id,))
+        step_thread.start()
+
+    app.run(host='0.0.0.0', port=5000)
---
+++ fyc/appv3.py
@@ -0,0 +1,177 @@
+from flask import Flask, request, jsonify
+import json
+import time
+import requests
+import torch
+import boto3
+import threading
+import logging
+import torchvision
+import torchvision.transforms as transforms
+import os
+
+session = boto3.Session(
+    aws_access_key_id='AKIAWZDD4R7ZCFG4H75Z',
+    aws_secret_access_key='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',
+    region_name='ap-northeast-1'  # 替换为实际的AWS区域
+)
+
+s3_client = session.client('s3')
+logging.basicConfig(level=logging.INFO)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def handler(event, context):
+
+    time_thread = threading.Thread(target=time_watcher, args=(event, context.function_name))
+    time_thread.start()
+
+    # TODO: train model
+
+    ip_address = event['ip_address']
+    port = event['port']
+    if_restart = event['if_restart']
+    bucket_name = event['bucket_name']
+    params_file_name = event['params_file_name']
+    group_id = event['group_id']
+    epochs_done = event['epoch']
+    
+    server_address = f"http://{ip_address}:{port}"
+
+    data_path = '/tmp/cifar10_data'
+
+    # 加载 CIFAR-10 数据集
+    train_dataset = torchvision.datasets.CIFAR10(
+        root=data_path,
+        train=True,
+        transform=transforms.ToTensor(),
+        download=True
+    )
+
+    # 创建数据加载器
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=64,
+        shuffle=True,
+        num_workers=2
+    )
+
+    # 创建 ResNet-18 模型
+    model = torchvision.models.resnet18(pretrained=False)
+
+    # if_restart
+    if (if_restart == True):
+        s3_client = boto3.client('s3')
+        s3_client.download_file(bucket_name, params_file_name, '/tmp/model_weights.pth')
+        model.load_state_dict(torch.load('/tmp/model_weights.pth'))
+    else:
+        epochs_done = 0
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(epochs_done, 20):
+        running_loss = 0.0
+        for i, (inputs, labels) in enumerate(train_loader, 0):
+            # 将输入和标签放入设备上
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            # 清零参数梯度
+            optimizer.zero_grad()
+
+            # 正向传播、反向传播、优化
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # 统计损失
+            running_loss += loss.item()
+            if i % 100 == 99:
+                print(f'Epoch: {epoch+1}, Batch: {i+1}, Loss: {running_loss/100:.3f}')
+                running_loss = 0.0
+
+        # 将模型权重发送给服务器并接收新权重
+        model_weights = model.state_dict()
+        message = {
+            'group_id' : group_id,
+            'request': 'average_step',
+            'step': model_weights
+        }
+        response = requests.post(server_address, json=message)
+        response_data = json.loads(response.text)
+
+        # 更新模型权重
+        new_model_weights = response_data.get('model_weights')
+        if new_model_weights:
+            model.load_state_dict(new_model_weights)
+
+        # 保存文件
+        file_name = '/tmp/model.pth'
+        torch.save(model.state_dict(), file_name)
+        with open('/tmp/epoch.txt', 'w') as file:
+            file.write(str(epoch))
+
+    return {
+        'statusCode': 200,
+        'body': model_weights
+    }
+
+
+def time_watcher(event, function_name):
+
+    ip_address = event['ip_address']
+    port = event['port']
+    if_restart = event['if_restart']
+    bucket_name = event['bucket_name']
+    params_file_name = event['params_file_name']
+    group_id = event['group_id']
+    epochs_done = event['epoch']
+    func_start = time.time()
+
+    time.sleep(60)
+
+    # Calculate the total time taken by the function
+    current_time = time.time()
+    total_time = current_time - func_start
+
+    while(total_time < 60):
+        current_time = time.time()
+        total_time = current_time - func_start 
+
+    # Save parameters to S3
+
+    with open('/tmp/epoch.txt', 'r') as file:
+        epoch = int(file.read())
+    PATH = '/tmp/model.pth'
+    #the place in s3
+    params_file_name = f'/tmp/{file_name}_{epoch}.pth'
+    
+    # Upload the file to the specified output bucket in S3
+    s3_client = boto3.client('s3')
+    s3_client.upload_file(PATH, bucket_name, params_file_name)
+
+    # Get server address
+    
+    server_address = f"http://{ip_address}:{port}"
+
+    data = {
+        'file_name' : function_name,
+        'request' : 'restart',
+        'ip_address' : ip_address,
+        'port' : port,
+        'bucket_name' : bucket_name,
+        'params_file_name' : params_file_name,
+        'group_id' : group_id,
+        'epochs_done' : epoch
+    }
+
+    # Send HTTP request
+    response = requests.post(server_address, json=data)
+
+    response_data = json.loads(response.text)
+
+    if response_data['message'] == 'CLOSE':
+        logging.info("CLOSE")
+    
+    return 0
+
